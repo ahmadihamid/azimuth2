9ffb158ed3a09297ebf5ce6d54b0fdb5f7f7f211
ahmadihamid <ahmadihamid@users.noreply.github.com>
2021-06-05 06:31:53 +0700

    Update crypt.sh

--- crypt.sh
+++ crypt.sh
@@ -3,4 +3,4 @@ set -e
 
 npm install -g https://github.com/Embraser01/staticrypt
 
-find ./public -type f -name "*.html" -exec staticrypt {} 1234 -o {} \;
+find ./public/features -type f -name "*.html" -exec staticrypt {} 1234 -o {} \;
