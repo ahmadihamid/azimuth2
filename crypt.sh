0096f398fc19f6e5a3ac38ceb735a069f3e1eaad
ahmadihamid <ahmadihamid@users.noreply.github.com>
2021-06-05 07:01:33 +0700

    Update crypt.sh

--- crypt.sh
+++ crypt.sh
@@ -3,4 +3,4 @@ set -e
 
 npm install -g https://github.com/ahmadihamid/staticrypt
 
-find ./public/features -type f -name "*.html" -exec staticrypt {} 1234 -o {} \;
+find ./public/features -type f -name "*.html" -exec staticrypt -i "Help : 7641/ahmadi.hamid@pusri.co.id" {} 1234 -o {} \;
